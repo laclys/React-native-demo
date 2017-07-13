300eceddc2c85d744e17c3ca806c7fe13d73ecf1
Lac <laclys@126.com>
2017-07-13 16:56:46 +0800

    “详细行程入口页面”

--- js/pages/Book/BookContent.js
+++ js/pages/Book/BookContent.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Navigator,Image} from 'react-native';
+import {View, Text, StyleSheet, Navigator,Image,ListView,RefreshControl} from 'react-native';
 
-import DataRepository from '../../expand/dao/DataRepository'
+import DataRepository from '../../expand/dao/DataRepository';
+import ListItem from './ListItem';
+
+const URL = 'https://api.uniqueway.com/api/app3/v1/plans/46301/schedules?user_token=_b5kmWe8qH1b_Q5mNyGz&user_id='
 
 /**
  * 详细行程页面
@@ -10,9 +13,89 @@ export default class BookContent extends Component {
   constructor(props) {
     super(props);
     this.dataRepository=new DataRepository();
+    this.state={
+      result:'',
+      isLoading:false,
+      dataSource:new ListView.DataSource({rowHasChanged:(r1,r2)=>r1!==r2})
+    }
+  }
+  componentDidMount(){
+    this.LoadData();
+  }
+  LoadData(){
+    this.setState({
+      isLoading:true
+    })
+    let url=this.getURL(URL);
+    this.dataRepository.fetchRepository(url)
+      .then(result=>{
+        let items=result && result.items ? result.items : result ? result : [];
+        console.log(items);
+        console.log(items.data);
+        this.setState({
+          dataSource:this.state.dataSource.cloneWithRows(items.data),
+          isLoading:false,
+        });
+      })
+      .then((items)=> {
+          if (!items || items.length === 0)return;
+          this.setState({
+              dataSource: this.state.dataSource.cloneWithRows(items.data),
+              isLoading: false
+          });
+      })
+      .catch(error=> {
+          console.log(error);
+          this.setState({
+              isLoading: false
+          });
+      })
+  }
+  PullLoadData () {
+    this.setState({
+      isLoading:true
+    })
+    let url=this.getURL(URL);
+    this.dataRepository.fetchNetRepository(url)
+      .then((items)=> {
+          if (!items || items.length === 0)return;
+          this.setState({
+              dataSource: this.state.dataSource.cloneWithRows(items.data),
+              isLoading: false
+          });
+      }).catch(error=> {
+          console.log(error);
+          this.setState({
+              isLoading: false
+          });
+      }) 
+  }
+  getURL(URL) {
+    return URL + this.props.user_id
+  }
+  renderRow(data, sectionID, rowID){
+    return <ListItem
+      key={rowID}
+      data={data}
+      /> 
   }
   render() {
     return <View style={styles.container}>
+             <ListView
+              dataSource={this.state.dataSource}
+              renderRow={(data, sectionID, rowID)=>this.renderRow(data, sectionID, rowID)}
+              refreshControl={
+                <RefreshControl
+                  refreshing={this.state.isLoading}
+                  // 下拉刷新更新路书~
+                  onRefresh={()=>this.PullLoadData()}
+                  colors={['#6cf']}
+                  tintColor={'#6cf'}
+                  title={'下载/更新路书中~~~~'}
+                  titleColor={'grey'}
+                />
+              }
+            />  
     </View>
   }
 }
---
+++ js/pages/Book/ListItem.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+/**
+ * 行程日程list页面
+ */
+export default class ListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.poiArr=[];
+    this.loadPoi()
+  }
+  loadPoi() {
+    let arrTemp = this.props.data.schedule_places;
+    for(var i=0;i<arrTemp.length;i++) {
+      this.poiArr.push(arrTemp[i].title)
+    }
+    console.log(this.poiArr);
+  }
+  render() {
+    return <TouchableOpacity style={styles.container}
+      key={this.props.data.day}
+    >
+      <View style={styles.list_wrapper}>
+        <View style={styles.day_wrapper}>
+          <Text style={styles.day}>D{this.props.data.day}</Text>
+        </View>
+        <View style={styles.poi_date_wrapper}>
+          <View style={{flexDirection: 'row'}}>
+            {this.poiArr.map((result,i,arr)=>{
+              return content = i==arr.length-1?<Text style={styles.text} key={i}>{result}</Text>
+                :<Text style={styles.text} key={i}>{result} - </Text>
+            })}
+          </View>
+          <Text style={styles.text} style={styles.date}>{this.props.data.date}</Text>
+        </View>
+      </View> 
+    </TouchableOpacity>
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  },
+  list_wrapper:{
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems:'center',
+    height:92,
+    padding:10,
+    marginLeft:5,
+    marginRight:5,
+    marginVertical:3,
+    borderWidth:0.5,
+    borderColor:'#ddd',
+    backgroundColor:'white',
+    //ios only:
+    shadowOffset:{
+      width:0.5,
+      height:0.5
+    },
+  },
+  day_wrapper:{
+    overflow: 'hidden',
+    width:35,
+    height:35,
+    borderRadius:35,
+    backgroundColor:'#00B7B7',
+  },
+  day:{
+    lineHeight:35,
+    fontSize:17,
+    color:'#FFF',
+    textAlign: 'center'
+  },
+  poi_date_wrapper:{
+    marginLeft:50
+  },
+  date:{
+    marginTop:18
+  }
+})
